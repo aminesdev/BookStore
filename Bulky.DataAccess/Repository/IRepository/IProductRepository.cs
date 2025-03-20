476d67cf81101736ebfc262bef4f3a6970ab1258
Amine Boughaba <boughabaamine76@gmail.com>
2025-03-20 14:43:18 +0100

    CRUDS operations

---
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,22 @@
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=> 
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
 
 var app = builder.Build();
 
@@ -22,8 +37,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
